cb15034c571c67e40a6a5319d6af82c47d875caf
Emanoel Lopes <emanoelopes@gmail.com>
2019-06-24 17:04:06 -0300

    Created user model, add links on navbar, installed hirb gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Bartosz Tieadman", password:"password")
+User.create(username: "Magnus Nielsen", password:"password")
+User.create(username: "Joanas Kahnwald", password:"password")
+User.create(username: "Charlotte Doppler", password:"password")
+User.create(username: "Martha Nielsen", password:"password")
